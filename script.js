cb31b1b5aa51e520a344151533d0cd836532dcbb
Nasib7 <nasibfarhan60@gmail.com>
2026-06-14 12:28:19 +0600

    Created repo for web dev practice. Added files for a simple webpage with a chart and some styling.

---
+++ script.js
@@ -0,0 +1,51 @@
+//personal info dashboard
+
+let personalInfoHeader = "Personal Information";
+document.getElementById("personal-info-header").innerHTML = `<i class="fa-solid fa-person"></i> ${personalInfoHeader}`;
+
+let title = "NASIB'S LIFE DASHBOARD";
+document.getElementById("title").innerHTML = `${title}`;
+
+let name = "Nasib Rahman Mazumder";
+document.getElementById("name").innerHTML = `Name: ${name}`;
+console.log(name);
+
+let age = "23";
+document.getElementById("age").innerHTML = `Age: ${age}`;
+console.log(age);
+
+let goal = "To see life clearly without judgement and filters.";
+document.getElementById("goal").innerHTML = `Goal: ${goal}`;
+
+let game = "Red Dead Redemption 2";
+document.getElementById("game").innerHTML = `Game: ${game}`;
+
+let streak = "626 days";
+document.getElementById("streak").innerHTML = `Streak: ${streak}`;
+
+let hobby = "Playing guitar";
+document.getElementById("hobby").innerHTML = `Hobby: ${hobby}`;
+
+let dream = "To study in t10 schools in the US";
+document.getElementById("dream").innerHTML = `Dream: ${dream}`;
+
+let quote = "Whatever happens, happens.";
+document.getElementById("quote").innerHTML = `Favorite Quote: ${quote}`;
+
+//work info dashboard
+
+let workInfoHeader = "Work Information";
+document.getElementById("work-info-header").innerHTML = `<i class="fa-solid fa-briefcase"></i> ${workInfoHeader}`;
+
+let company = "ARCED Foundation";
+document.getElementById("company").innerHTML = `Comapany: ${company}`;
+
+let position = "Intern";
+document.getElementById("position").innerHTML = `Position: ${position}`;
+
+let date = "2024-06-07";
+document.getElementById("date").innerHTML = `Start Date: ${date}`;
+
+let duration = "3 months";
+document.getElementById("duration").innerHTML = `Duration: ${duration}`;
+
